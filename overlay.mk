398a4dd7c653e88ea337578af0db1e1c416cf86c
khrO0t <katohack202@gmail.com>
2025-07-31 19:27:52 +0300

    Added files for motorola Edge 50 neo (vienna)

--- overlay.mk
+++ overlay.mk
@@ -133,6 +133,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-moto-tesla-systemui \
 	treble-overlay-moto-tundra \
 	treble-overlay-moto-tundra-systemui \
+	treble-overlay-moto-vienna \
 	treble-overlay-mtk-ims \
 	treble-overlay-nokia-b2n-7plus \
 	treble-overlay-nokia-ctl-7-1 \
